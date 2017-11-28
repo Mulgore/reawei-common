ccb45abee80ec5056a4469c3ca88e5ac110e6bf9
xingwus <xingwu@ixiye.com>
2017-11-28 16:19:17 +0800

    update

--- src/main/java/cn/reawei/enums/BaiDu.java
+++ src/main/java/cn/reawei/enums/BaiDu.java
@@ -1,9 +1,11 @@
 package cn.reawei.enums;
 
 
+/**
+ * 百度地图AK
+ */
 public enum BaiDu {
     // "百度地图Ak"
-    BAI_DU_AK("w1yteHTTXUzLPfCVnm5Y9sYo"),
     BAI_DU_AK2("R62KkaKOTkOHC93qDWipbg0sON1Pwvc6");
 
     private String ak;
