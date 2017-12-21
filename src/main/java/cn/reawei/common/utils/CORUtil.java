91172edfcc3c6e0099227b40fb606093a80dc78d
xingwus <xingwu@ixiye.com>
2017-12-21 14:46:31 +0800

    update

--- src/main/java/cn/reawei/common/utils/CORUtil.java
+++ src/main/java/cn/reawei/common/utils/CORUtil.java
@@ -29,9 +29,31 @@ public class CORUtil {
     }
 
     /**
+     * @param imgPath 本地图片路径
+     */
+    public static ResultBean getBankNumber(String imgPath, AipOcr client) {
+        // 调用接口
+        JSONObject response = client.bankcard(imgPath);
+        return getResultBeanByBankCard(response);
+    }
+
+    /**
      * @param imgUrl 网络图片路径
      */
     public static ResultBean getBankNumberByImageUrl(String imgUrl) {
+        // 调用接口
+        byte[] file = ImageUtil.getImageFromNetByUrl(imgUrl);
+        JSONObject response = new JSONObject();
+        if (Objects.nonNull(file)) {
+            response = client.bankcard(file);
+        }
+        return getResultBeanByBankCard(response);
+    }
+
+    /**
+     * @param imgUrl 网络图片路径
+     */
+    public static ResultBean getBankNumberByImageUrl(String imgUrl, AipOcr client) {
         // 调用接口
         byte[] file = ImageUtil.getImageFromNetByUrl(imgUrl);
         JSONObject response = new JSONObject();
@@ -52,10 +74,32 @@ public class CORUtil {
     }
 
     /**
+     * @param inStream 输入流
+     */
+    public static ResultBean getBankNumberBySteam(InputStream inStream, AipOcr client) throws Exception {
+        // 调用接口
+        byte[] file = ImageUtil.readInputStream(inStream);
+        JSONObject response = client.bankcard(file);
+        return getResultBeanByBankCard(response);
+    }
+
+    /**
      * @param path    本地图片
      * @param isFront 是否正面
      */
     public static ResultBean getIDCard(String path, boolean isFront) {
+        // 调用接口
+        byte[] file = ImageUtil.readImageFile(path);
+        HashMap<String, String> options = new HashMap<>();
+        JSONObject response = client.idcard(file, isFront, options);
+        return getResultBeanByIDCard(isFront, response);
+    }
+
+    /**
+     * @param path    本地图片
+     * @param isFront 是否正面
+     */
+    public static ResultBean getIDCard(String path, boolean isFront, AipOcr client) {
         // 调用接口
         byte[] file = ImageUtil.readImageFile(path);
         HashMap<String, String> options = new HashMap<>();
@@ -76,7 +120,21 @@ public class CORUtil {
         if (Objects.nonNull(file)) {
             response = client.idcard(file, isFront, options);
         }
+        return getResultBeanByIDCard(isFront, response);
+    }
 
+    /**
+     * @param imgUrl  网络图片
+     * @param isFront 是否正面
+     */
+    public static ResultBean getIDCardByImageUrl(String imgUrl, boolean isFront, AipOcr client) {
+        // 调用接口
+        byte[] file = ImageUtil.getImageFromNetByUrl(imgUrl);
+        HashMap<String, String> options = new HashMap<>();
+        JSONObject response = new JSONObject();
+        if (Objects.nonNull(file)) {
+            response = client.idcard(file, isFront, options);
+        }
         return getResultBeanByIDCard(isFront, response);
     }
 
@@ -85,6 +143,18 @@ public class CORUtil {
      * @param isFront  是否正面
      */
     public static ResultBean getIDCardByStream(InputStream inStream, boolean isFront) throws Exception {
+        // 调用接口
+        byte[] file = ImageUtil.readInputStream(inStream);
+        HashMap<String, String> options = new HashMap<>();
+        JSONObject response = client.idcard(file, isFront, options);
+        return getResultBeanByIDCard(isFront, response);
+    }
+
+    /**
+     * @param inStream 输入流
+     * @param isFront  是否正面
+     */
+    public static ResultBean getIDCardByStream(InputStream inStream, boolean isFront, AipOcr client) throws Exception {
         // 调用接口
         byte[] file = ImageUtil.readInputStream(inStream);
         HashMap<String, String> options = new HashMap<>();
@@ -171,7 +241,7 @@ public class CORUtil {
         JSONObject result = response.getJSONObject("result");
         JSONObject data = new JSONObject();
         if (!result.isNull("bank_card_number")) {
-            data.put("number", result.getString("bank_card_number").replaceAll(" ",""));
+            data.put("number", result.getString("bank_card_number").replaceAll(" ", ""));
         }
         if (!result.isNull("bank_card_type")) {
             data.put("type", result.getInt("bank_card_type"));
@@ -184,9 +254,10 @@ public class CORUtil {
 
     public static void main(String[] args) {
 //        String path = "/Users/xingwu/Downloads/bankCard.jpg";
+        AipOcr client = new AipOcr(BAI_DU_COR_APP_ID.key, BAI_DU_COR_API_KEY.key, BAI_DU_COR_SECRET_KEY.key);
         String path = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513836045361&di=31f5c0f4ac5631712eea1637fc0b1948&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fzhidao%2Fwh%253D450%252C600%2Fsign%3D6665b20d0a24ab18e043e93300cacafb%2F3b292df5e0fe992588323e7b37a85edf8db171da.jpg";
 //        String path = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513846741878&di=8d06c6855d73ba0f77f5d07f820c722d&imgtype=0&src=http%3A%2F%2Fwww.yktchina.com%2FFileUpLoad%2F2009%2Fb2d696c0b1b74521b1af0275cd286121.jpg";
-        System.out.println(getBankNumberByImageUrl(path).getData().toString());
+        System.out.println(getBankNumberByImageUrl(path, client).getData().toString());
 //        String urlPath = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513837657145&di=2fd4fb799bb6fba839861ac288e89fa5&imgtype=0&src=http%3A%2F%2Fimg.chinawutong.com%2Fhuiyuan%2Fuppic%2Fs_636054904236200186.png";
 
 //        System.out.println(getIDCard(path, true));
